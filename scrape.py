8ba36f9eec8304a4c8f07d615047b2abae166975
Mwiza Simbeye <mwizasimbeye@gmail.com>
2017-04-04 02:44:54 -0800

    Iterration of rows added

--- scrape.py
+++ scrape.py
@@ -1,5 +1,13 @@
+import os
 import pandas as pd
 
 # Read dataset, 
 corn_data = pd.read_csv('datasets/corn_maize_images.csv', error_bad_lines=False)
+
+plant_disease_name = corn_data[['Disease common name']]
+plant_disease_name = plant_disease_name.fillna('Normal')
+plant_urls = corn_data[['url']]
+
+for x in plant_disease_name.iterrows():
+	print x
 
